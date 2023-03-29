8ce984eb09d9bdbdca18295ce9db8d5862f6e857
Taras <kill_pro@ukr.net>
2023-03-29 18:39:40 +0300

    add lesson 29

---
+++ lesson-29(start-js)/main.js
@@ -0,0 +1,28 @@
+"use strict";
+
+// const name;
+// const surname;
+
+// const userName;
+// const userSecondName;
+
+const CURRENT_YEAR = 2023;
+
+const userFirstName = prompt("Як тебе звати?", "");
+alert(`Привіт, ${userFirstName}`);
+
+// const userBurnYear = prompt("Якого ти року народження?", "");
+// const userAge = CURRENT_YEAR - userBurnYear;
+// alert(userAge);
+
+// const sideSquare = prompt("Яка довжина сторони вашого квадрата?", "");
+// const perimeter = 4 * sideSquare;
+// alert(`площа вашого квадрата - ${perimeter}`);
+
+// способи коментування :
+
+/*
+jfdskksf
+*/
+
+//
