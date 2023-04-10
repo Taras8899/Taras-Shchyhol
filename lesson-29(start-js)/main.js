807c18efd879345e8eb5d1341236cd5a0ac8bf96
Taras <kill_pro@ukr.net>
2023-04-10 17:31:12 +0300

    add lesson30

--- lesson-29(start-js)/main.js
+++ lesson-29(start-js)/main.js
@@ -1,28 +1,28 @@
 "use strict";
 
-// const name;
-// const surname;
+// // const name;
+// // const surname;
 
-// const userName;
-// const userSecondName;
+// // const userName;
+// // const userSecondName;
 
-const CURRENT_YEAR = 2023;
+// const CURRENT_YEAR = 2023;
 
-const userFirstName = prompt("Як тебе звати?", "");
-alert(`Привіт, ${userFirstName}`);
+// const userFirstName = prompt("Як тебе звати?", "");
+// alert(`Привіт, ${userFirstName}`);
 
-// const userBurnYear = prompt("Якого ти року народження?", "");
-// const userAge = CURRENT_YEAR - userBurnYear;
-// alert(userAge);
+// // const userBurnYear = prompt("Якого ти року народження?", "");
+// // const userAge = CURRENT_YEAR - userBurnYear;
+// // alert(userAge);
 
-// const sideSquare = prompt("Яка довжина сторони вашого квадрата?", "");
-// const perimeter = 4 * sideSquare;
-// alert(`площа вашого квадрата - ${perimeter}`);
+// // const sideSquare = prompt("Яка довжина сторони вашого квадрата?", "");
+// // const perimeter = 4 * sideSquare;
+// // alert(`площа вашого квадрата - ${perimeter}`);
 
-// способи коментування :
+// // способи коментування :
 
-/*
-jfdskksf
-*/
+// /*
+// jfdskksf
+// */
 
-//
+// //
---
+++ lesson-30(data types)/main.js
@@ -0,0 +1,31 @@
+"use strict";
+
+const num1 = 0.1;
+
+const num2 = 0.2;
+
+const result = num1 + num2;
+
+alert(result);
+
+// const numOne = "1";
+// const numTwo = 2;
+
+// alert(+numOne + numTwo);
+
+// const file = 820;
+// const userFile = +prompt("на скільки мегабайт у вас флешка?", "");
+// const quantityFile = userFile / file;
+// alert(`на вашу флешку поміститься - ${quantityFile} файлів`);
+
+// const sum = +prompt("Введіть суму на яку хочете придбати", "");
+
+// const price = +prompt("Введіть ціну шоколадки", "");
+
+// const quantityChocolate = sum / price;
+
+// const remainder = sum % price;
+
+// alert(
+//   `Ви зможете купити ${quantityChocolate} шоколадок , ваша решта ${remainder}`
+// );
